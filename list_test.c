712d5f43ea323b4b4625270653a288b3c9314e45
Sean Kugele <kugeles@rhodes.edu>
2026-04-01 10:33:39 -0500

    Add insert_at and find to list api

--- list_test.c
+++ list_test.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "list.h"
+
+void display_list(list_t *list);
+node_t *find(list_t *list, char *value);
 
 int main(void) {
   list_t *list = create_list();
 
-  int i_array[5] = {1,2,3,4};
+  int i_array[5] = {1, 2, 3, 4, 5};
 
   list->end = append(list, create_node("first", &i_array[0]));
   list->end = append(list, create_node("second", &i_array[1]));
-  list->end = append(list, create_node("third", &i_array[2]));
   list->end = append(list, create_node("fourth", &i_array[3]));
+  list->end = append(list, create_node("fifth", &i_array[4]));
 
-  int *value = NULL;
+  node_t *parent = find(list, "second");
+
+  if (parent != NULL) {
+    insert_at(list, parent, create_node("third", &i_array[2]));
+  }
+
+  display_list(list);
+
+  /* TODO: remove all nodes and free memory */
+
+  return 0;
+}
+
+node_t *find(list_t *list, char *value) {
+  /* TODO: insert nodes in the middle */
+  node_t *parent = NULL;
+  node_t *curr = NULL;
+
+  while (curr != NULL) {
+    if (strcmp(curr->data, "second") == 0){
+      parent = curr;
+      break;
+    }
+  }
+
+  return parent;
+}
+
+void display_list(list_t *list) {
+  /* displays the linked list from start to end */
   node_t *curr = list->start;
 
   while (curr != NULL) {
-    value = curr->data;
+    int *value = curr->data;
     printf("%s = %d\n", curr->id, *value);
+
     curr = curr->next;
   }
 
-  return 0;
+  printf("# elements: %d\n", list->size);
 }
